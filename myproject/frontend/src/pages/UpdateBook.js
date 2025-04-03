e70728c61a2b114abac4e77fe810e372d0cbc54b
= <=>
2025-04-04 02:12:15 +0530

    new

--- myproject/frontend/src/pages/UpdateBook.js
+++ myproject/frontend/src/pages/UpdateBook.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
+import "../styles/UpdateBook.css";
+
 const UpdateBook = () => {
   const { id } = useParams();
   const navigate = useNavigate();
